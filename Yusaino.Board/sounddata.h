3c12c08407e41b058fda7ebf9fb3d357b66d3e3e
George Wu <uiharu@buaa.edu.cn>
2016-12-30 00:37:40 +0100

    添加项目文件。

---
+++ Yusaino.Board/sounddata.h
@@ -0,0 +1,11 @@
+#ifndef __ARDUINO_SOUND_DATA__
+#define __ARDUINO_SOUND_DATA__
+
+extern const uint_fast32_t SampleRate;
+extern const int_fast8_t SampleBits;
+
+extern int_fast16_t const HuffDict[];
+extern uint_fast32_t const SoundDataBits;
+extern uint8_t const SoundData[];
+
+#endif
